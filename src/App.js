7d9c27111d7586f038ccb68a2a7bd35b62f29de9
Bikram Upadhyay <upadhyaybikram@gmail.com>
2024-01-15 22:38:20 +1100

    Header, Movie and Search component for fetching movies lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useReducer, useEffect } from "react";
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+import Search from "./components/Search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
+const MOVIE_API_URL_1 = "http://www.omdbapi.com/?i=tt3896198&apikey=95e5f071";
+
+const initialState = {
+    loading: true,
+    movies: [],
+    errorMessage: null
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SEARCH_MOVIES_REQUEST":
+            return {
+                ...state,
+                loading: true,
+                errorMessage: null
+            };
+        case "SEARCH_MOVIES_SUCCESS":
+            return {
+                ...state,
+                loading: false,
+                movies: action.payload
+            };
+        case "SEARCH_MOVIES_FAILURE":
+            return {
+                ...state,
+                loading: false,
+                errorMessage: action.error
+            };
+        default:
+            return state;
+    }
+};
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        fetch(MOVIE_API_URL)
+            .then((response) => response.json())
+            .then((jsonResponse) => {
+                console.log("Movie API URL Response:", jsonResponse);
+                dispatch({
+                    type: "SEARCH_MOVIES_SUCCESS",
+                    payload: jsonResponse.Search
+                });
+            });
+    }, []);
+
+    const search = (searchValue) => {
+        dispatch({
+            type: "SEARCH_MOVIES_REQUEST"
+        });
+
+        fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+            .then((response) => response.json())
+            .then((jsonResponse) => {
+                if (jsonResponse.Response === "True") {
+                    dispatch({
+                        type: "SEARCH_MOVIES_SUCCESS",
+                        payload: jsonResponse.Search
+                    });
+                } else {
+                    dispatch({
+                        type: "SEARCH_MOVIES_FAILURE",
+                        error: jsonResponse.Error
+                    });
+                }
+            });
+    };
+
+    const { movies, errorMessage, loading } = state;
+
+    return (
+        <div className="App">
+            <Header text="MOVIE APP" />
+            <Search search={search} />
+            <p className="App-intro">Find your favourite movie here</p>
+            <div className="movies">
+                {loading && !errorMessage ? (
+                    <span>loading... </span>
+                ) : errorMessage ? (
+                    <div className="errorMessage">{errorMessage}</div>
+                ) : (
+                    movies.map((movie, index) => (
+                        <Movie key={`${index}-${movie.Title}`} movie={movie} />
+                    ))
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = (props) => {
+    return (
+        <header className="App-header">
+            <h2>{props.text}</h2>
+        </header>
+    );
+};
+
+export default Header;
