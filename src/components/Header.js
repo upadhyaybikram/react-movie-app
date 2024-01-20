a02923e9b81117f88480c6091352ad876df7c6aa
Bikram Upadhyay <upadhyaybikram@gmail.com>
2024-01-20 21:46:27 +1100

    #on click on header component, reloads the page #header component modified to accept text and onClick as props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
-const Header = (props) => {
+// const Header = (props) => {
+//     return (
+//         <header className="App-header">
+//             <h2>{props.text}</h2>
+//         </header>
+//     );
+// };
+//
+// export default Header;
+
+
+const Header = ({text, onClick}) => {
     return (
-        <header className="App-header">
-            <h2>{props.text}</h2>
+        <header className="App-header" onClick={onClick}>
+            <h2>{text}</h2>
         </header>
     );
 };
